631f0c355d4e3debfb488c4320aa6d108dff329a
Nico Arato <nico.arato@gmail.com>
2020-04-02 19:30:32 -0300

    Observer y subscriber.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
+import {  Observable, Subscriber, Observer  } from 'rxjs';
+
+const observer:Observer<any> = {
+    next: value => console.log('siguiente', value),
+    error: error=> console.warn('error [observer: ', error),
+    complete: () => console.info('Completo- [observer]')
+
+}
+
+const obs$ = new Observable<string>( subs => {
+
+    subs.next('Hola');
+    subs.next('mundo');
+
+    //forzar un error
+    /* const a = undefined;
+    a.nombre = 'Carlos'; */
+
+    subs.complete();
+
+    subs.next('Hola');
+    subs.next('mundo');
 
 
-console.log('Hola Mundo!');
+});
 
+obs$.subscribe( observer );
 
-
-
-
+/* obs$.subscribe(
+    valor => console.log('next: ', valor),
+    error => console.warn('error: ', error),
+    () => console.log('Completado')
+); */
 
 
