98d0bbde3325ff11059261a7b6d39d52096b0092
Nico Arato <nico.arato@gmail.com>
2020-04-13 02:47:50 -0300

    Distinct

---
+++ src/operadores/11-distinct.ts
@@ -0,0 +1,37 @@
+import { of, from } from 'rxjs';
+import { distinct } from 'rxjs/operators';
+
+
+const numeros$ = of( 1,1,1,3,3,3,2,4,5,6,3,1 );
+numeros$.pipe(
+    distinct()// USA === OJO
+).subscribe({
+    next: val => console.log('next: ', val),
+    complete: () => console.log('Complete')
+})
+
+interface Personaje {
+    nombre: string;
+}
+
+const personajes: Personaje[] = [
+    {
+        nombre: 'Megaman'
+    },
+    {
+        nombre: 'Zero'
+    },
+    {
+        nombre: 'Dr Wally'
+    },
+    {
+        nombre: 'Megaman'
+    },
+    {
+        nombre: 'Zero'
+    },
+];
+
+from( personajes).pipe(
+    distinct( p => p.nombre) //para objetos.
+).subscribe( console.log );
